9d14e0e3b96b7e4f8f7a7dd0c7320177d83e6128
Stephen Crane <jscrane@gmail.com>
2014-10-16 19:22:29 +0100

    more optimisation

--- lm4f.mk
+++ lm4f.mk
@@ -1,8 +1,8 @@
 CPUFLAGS = -mthumb -mcpu=$(BUILD_MCU) -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant
-CFLAGS = -O0 -Wall -ffunction-sections -fdata-sections $(CPUFLAGS)
+CFLAGS = -O3 -Wall -ffunction-sections -fdata-sections $(CPUFLAGS)
 CXXFLAGS = -fno-rtti -fno-exceptions $(CFLAGS)
 LD_SCRIPT = $(shell sed -ne "s/$(BOARD).ldscript=\(.*\)/\1/p" $(BOARDS))
-LDFLAGS = -O0 -nostartfiles -nostdlib -Wl,--gc-sections,--entry=ResetISR -T $(PROCESSOR_CORE)/$(LD_SCRIPT) $(CPUFLAGS)
+LDFLAGS = -Os -nostartfiles -nostdlib -Wl,--gc-sections,--entry=ResetISR -T $(PROCESSOR_CORE)/$(LD_SCRIPT) $(CPUFLAGS)
 LD = $(COMPILER_FAMILY)-g++
 OBJCOPY_FLAGS = -O binary
 UPLOADER = lm4flash 
