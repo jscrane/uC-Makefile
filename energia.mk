938f2f230d51273f4a64e80ea26164a7eb796f7d
Stephen Crane <jscrane@gmail.com>
2016-02-20 17:20:56 +0000

    ...

--- energia.mk
+++ energia.mk
@@ -2,7 +2,7 @@ IDE_HOME ?= /usr/local/energia
 SKETCHBOOK ?= $(HOME)/energia
 PLATFORM := $(PROCESSOR_FAMILY)
 PLATFORM_HEADER := Energia.h
-CPPFLAGS := -DENERGIA=13 -DARDUINO=105 
+CPPFLAGS += -DENERGIA=13 -DARDUINO=105
 LIBRARIES := $(SKETCHBOOK)/libraries $(IDE_HOME)/hardware/$(PROCESSOR_FAMILY)/libraries
 
 include ucmk.mk
