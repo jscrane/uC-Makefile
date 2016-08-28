dbcc573d5b05cb37cb630d88d8676bf11304e0e6
Stephen Crane <jscrane@gmail.com>
2016-08-28 14:15:12 +0100

    bugfixes

--- arduino-tiny.mk
+++ arduino-tiny.mk
@@ -2,7 +2,7 @@ IDE_HOME ?= /usr/local/arduino
 SKETCHBOOK ?= $(HOME)/sketchbook
 UPLOAD_PORT ?= /dev/ttyUSB0
 PROCESSOR_FAMILY := avr
-PLATFORM := $(PROCESSOR_FAMILY)
+PLATFORM := arduino
 HARDWARE_FAMILY := $(SKETCHBOOK)/hardware/attiny/avr
 CORE := $(IDE_HOME)/hardware/arduino/avr/cores/arduino
 
--- arduino15.mk
+++ arduino15.mk
@@ -2,7 +2,7 @@ IDE_HOME ?= /usr/local/arduino
 SKETCHBOOK ?= $(HOME)/sketchbook
 UPLOAD_PORT ?= /dev/ttyUSB0
 PROCESSOR_FAMILY ?= avr
-PLATFORM := $(PROCESSOR_FAMILY)
+PLATFORM := arduino
 HARDWARE_FAMILY ?= $(IDE_HOME)/hardware/$(PLATFORM)/$(PROCESSOR_FAMILY)
 
 LIBRARIES := $(SKETCHBOOK)/libraries $(HARDWARE_FAMILY)/libraries $(IDE_HOME)/libraries
@@ -10,4 +10,4 @@ PLATFORM_HEADER := Arduino.h
 
 EXTRA_TARGETS := eep size
 include ucmk.mk
-include avr-targets.mk
+include targets-avr.mk
