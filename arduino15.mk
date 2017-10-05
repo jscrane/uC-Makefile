160423e16b379dc7df2af710ccb6a48f99a8abe7
Stephen Crane <jscrane@gmail.com>
2017-10-05 09:25:52 +0100

    versions

--- arduino15.mk
+++ arduino15.mk
@@ -2,12 +2,10 @@ IDE_HOME ?= /usr/local/arduino
 SKETCHBOOK ?= $(HOME)/sketchbook
 UPLOAD_PORT ?= /dev/ttyUSB0
 PROCESSOR_FAMILY ?= avr
-CPPFLAGS += -DARDUINO=105
 PLATFORM := arduino
-HARDWARE_FAMILY ?= $(IDE_HOME)/hardware/$(PLATFORM)/$(PROCESSOR_FAMILY)
-
-LIBRARIES := $(SKETCHBOOK)/libraries $(HARDWARE_FAMILY)/libraries $(IDE_HOME)/libraries
 PLATFORM_HEADER := Arduino.h
+CPPFLAGS += -DARDUINO=10804
+LIBRARIES := $(SKETCHBOOK)/libraries $(HARDWARE_FAMILY)/libraries $(IDE_HOME)/libraries
 
 EXTRA_TARGETS := eep size
 include ucmk.mk
--- energia.mk
+++ energia.mk
@@ -2,7 +2,7 @@ IDE_HOME ?= /usr/local/energia
 SKETCHBOOK ?= $(HOME)/energia
 PLATFORM := energia
 PLATFORM_HEADER := Energia.h
-CPPFLAGS += -DENERGIA=13 -DARDUINO=105
+CPPFLAGS += -DENERGIA=10610 -DARDUINO=10610
 LIBRARIES := $(SKETCHBOOK)/libraries $(HARDWARE_FAMILY)/libraries
 
 include ucmk.mk
