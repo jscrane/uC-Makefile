8de47e9514f618eac5963e7dd5289d85f6cce0f1
Stephen Crane <jscrane@gmail.com>
2018-03-09 15:20:37 +0000

    fix spiffs

--- targets-esp.mk
+++ targets-esp.mk
@@ -1,10 +1,12 @@
 SPIFFS_SIZE := $(shell echo $$(( $(SPIFFS_END) - $(SPIFFS_START) )))
 IMAGE ?= spiffs.img
 
-fs:
-	mkspiffs --create $(SPIFFS_DIR) -s $(SPIFFS_SIZE) $(IMAGE)
+$(IMAGE):
+	mkspiffs -c $(SPIFFS_DIR) -b $(SPIFFS_BLOCKSIZE) -p $(SPIFFS_PAGESIZE) -s $(SPIFFS_SIZE) $(IMAGE)
 
-upload-fs:
+fs: $(IMAGE)
+
+upload-fs: $(IMAGE)
 	esptool -cd $(UPLOAD_RESET) -cb $(UPLOAD_SPEED) -cp $(UPLOAD_PORT) -ca $(SPIFFS_START) -cf $(IMAGE)
 
 reset:
