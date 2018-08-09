3b450c74ecea8c319ec4d9dfa4d12a783502c524
Stephen Crane <jscrane@gmail.com>
2018-08-09 13:48:22 +0100

    clean partitions

--- targets-esp32.mk
+++ targets-esp32.mk
@@ -3,12 +3,12 @@ IMAGE ?= spiffs.img
 $(IMAGE): $(wildcard $(SPIFFS_DIR)/*)
 	mkspiffs -c $(SPIFFS_DIR) -b $(SPIFFS_BLOCKSIZE) -p $(SPIFFS_PAGESIZE) -s $(SPIFFS_SIZE) $(IMAGE)
 
-.PHONY: fs upload-fs partitions
+$(SKETCH_PARTITIONS):
+	$(TOOL_DIR)/gen_esp32part.py -q $(PARTITIONS) $(SKETCH_PARTITIONS)
+
+.PHONY: fs upload-fs
 
 fs: $(IMAGE)
 
 upload-fs: $(IMAGE)
 	$(UPLOAD_TOOL) --chip esp32 --baud $(UPLOAD_SPEED) --port $(UPLOAD_PORT) --before default_reset --after hard_reset write_flash -z --flash_mode $(FLASH_MODE) --flash_freq $(FLASH_FREQ) --flash_size detect $(SPIFFS_START) $(IMAGE)
-
-partitions:
-	$(TOOL_DIR)/gen_esp32part.py -q $(PARTITIONS) $(SKETCH_PARTITIONS)
