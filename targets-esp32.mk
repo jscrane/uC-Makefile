358afdd36c42b21474345497aaaff9b57a47160a
Stephen Crane <jscrane@gmail.com>
2018-11-20 14:07:12 +0000

    bugfix

--- targets-esp32.mk
+++ targets-esp32.mk
@@ -6,7 +6,7 @@ $(SKETCH_EEP):
 
 .PHONY: fs upload-fs
 
-fs: $(IMAGE)
+fs: $(SPIFFS_IMAGE)
 
 upload-fs: $(SPIFFS_IMAGE)
 	$(UPLOAD_TOOL) --chip esp32 --baud $(UPLOAD_SPEED) --port $(UPLOAD_PORT) --before default_reset --after hard_reset write_flash -z --flash_mode $(FLASH_MODE) --flash_freq $(FLASH_FREQ) --flash_size detect $(SPIFFS_START) $(SPIFFS_IMAGE)
