586741181c4d1f546075f1190da0c4ed839e5b84
Stephen Crane <jscrane@gmail.com>
2018-11-25 11:57:12 +0000

    update

--- avr.mk
+++ avr.mk
@@ -1,22 +1,22 @@
 # device ids for avr include files: add more as needed
-P_atmega328p = __AVR_ATmega328P__
-P_atmega168 = __AVR_ATmega168__
-P_atmega2560 = __AVR_ATmega2560__
-P_atmega1280 = __AVR_ATmega1280__
-P_atmega32u4 = __AVR_ATmega32U4__
-P_atmega8 = __AVR_ATmega8__
-P_attiny84 = __AVR_ATtiny84__
-P_attiny85 = __AVR_ATtiny85__
+P_atmega328p := __AVR_ATmega328P__
+P_atmega168 := __AVR_ATmega168__
+P_atmega2560 := __AVR_ATmega2560__
+P_atmega1280 := __AVR_ATmega1280__
+P_atmega32u4 := __AVR_ATmega32U4__
+P_atmega8 := __AVR_ATmega8__
+P_attiny84 := __AVR_ATtiny84__
+P_attiny85 := __AVR_ATtiny85__
 
 # avrdude part ids: add more here as needed
-U_atmega328p = m328p
-U_atmega168 = m168
-U_atmega2560 = m2560
-U_atmega1280 = m1281
-U_atmega32u4 = m32u4
-U_atmega8 = m8
-U_attiny84 = t84
-U_attiny85 = t85
+U_atmega328p := m328p
+U_atmega168 := m168
+U_atmega2560 := m2560
+U_atmega1280 := m1281
+U_atmega32u4 := m32u4
+U_atmega8 := m8
+U_attiny84 := t84
+U_attiny85 := t85
 
 CPPFLAGS += -D${P_${BUILD_MCU}}
 CPUFLAGS += -mmcu=$(BUILD_MCU)
