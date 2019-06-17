3a89c2f2e0a93faa7eea06144290122068e738dd
Stephen Crane <jscrane@gmail.com>
2019-06-17 13:28:35 +0100

    boards.local.txt

--- tiny.mk
+++ tiny.mk
@@ -11,13 +11,6 @@ PROGRAMMER ?= arduinoasisp
 EESAVE ?= aenable
 BOD ?= 1v8
 
-ifndef BOARD_CHIP
-$(error BOARD_CHIP required)
-endif
-ifndef BOARD_CLOCK
-$(error BOARD_CLOCK required)
-endif
-
 VENDOR := ATTinyCore
 PROCESSOR_FAMILY := avr
 PACKAGES := $(HOME)/.arduino15/packages
@@ -34,20 +27,40 @@ runtime.tools.$(COMPILER_FAMILY).path := $(COMPILER_PATH)
 runtime.tools.avrdude.path := $(ARDUINO_TOOLS)/avrdude/6.3.0-arduino14
 
 -include boards.txt.mk
+include boards.local.txt.mk
 -include platform.txt.mk
 
 build.board := $(BOARD)
-BOARD_CPU_MENU := $(build.board).menu.chip.$(BOARD_CHIP)
-build.mcu := $($(BOARD_CPU_MENU).build.mcu)
+board.chip := $($(build.board).chip)
+board.chip ?= $(BOARD_CHIP)
+build.mcu := $($(build.board).build.mcu)
+BOARD_CPU_MENU := $(build.board).menu.chip.$(board.chip)
+build.mcu ?= $($(BOARD_CPU_MENU).build.mcu)
 build.core := $($(build.board).build.core)
-BOARD_CLOCK_MENU := $(build.board).menu.clock.$(BOARD_CLOCK)
-build.f_cpu := $($(BOARD_CLOCK_MENU).build.f_cpu)
+board.clock := $($(build.board).clock)
+board.clock ?= $(BOARD_CLOCK)
+build.f_cpu ?= $($(build.board).build.f_cpu)
+BOARD_CLOCK_MENU := $(build.board).menu.clock.$(board.clock)
+build.f_cpu ?= $($(BOARD_CLOCK_MENU).build.f_cpu)
 
-ifeq ($(BOARD_CHIP),85)
+ifndef BOARD
+$(error BOARD required)
+endif
+ifndef board.chip
+$(error BOARD_CHIP required)
+endif
+ifndef board.clock
+$(error BOARD_CLOCK required)
+endif
+
+ifeq ($(board.chip),85)
 build.variant := $($(build.board).build.variant)
 endif
-ifeq ($(BOARD_CHIP),84)
-build.variant := $($(build.board).menu.pinmapping.$(BOARD_PINMAPPING).build.variant)
+ifeq ($(board.chip),84)
+board.pinmapping := $($(build.board).pinmapping)
+build.variant := $($(build.board).build.variant)
+board.pinmapping ?= $(BOARD_PINMAPPING)
+build.variant ?= $($(build.board).menu.pinmapping.$(board.pinmapping).build.variant)
 endif
 
 CORE := $(runtime.platform.path)/cores/$(build.core)
@@ -65,12 +78,14 @@ program.extra_params := $(PROGRAM_EXTRA_PARAMS)
 erase.verbose := $(tools.$(upload.tool).erase.params.$(ERASE_VERBOSE))
 bootloader.verbose := $(tools.$(upload.tool).bootloader.params.$(BOOTLOADER_VERBOSE))
 bootloader.file := $($(build.board).bootloader.file)
-bootloader.low_fuses := $($(BOARD_CLOCK_MENU).bootloader.low_fuses)
+bootloader.low_fuses := $($(build.board).bootloader.low_fuses)
+bootloader.low_fuses ?= $($(BOARD_CLOCK_MENU).bootloader.low_fuses)
 bootloader.bod_bits := $($(build.board).menu.bod.$(BOD).bootloader.bod_bits)
 bootloader.eesave_bit := $($(build.board).menu.eesave.$(EESAVE).bootloader.eesave_bit)
 bootloader.high_fuses := $($(build.board).bootloader.high_fuses)
 bootloader.extended_fuses := $($(build.board).bootloader.extended_fuses)
 bootloader.lock_bits := $($(build.board).bootloader.lock_bits)
+bootloader.unlock_bits := $($(build.board).bootloader.unlock_bits)
 
 SKETCH_EEP = $(SKETCH_ELF:.elf=.eep)
 
