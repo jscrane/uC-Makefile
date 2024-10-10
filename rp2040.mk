1687768bbdaf323554fea526969aaa52ccaf98eb
steve <jscrane@gmail.com>
2024-10-10 14:01:50 +0100

    building littlefs image

--- rp2040.mk
+++ rp2040.mk
@@ -3,6 +3,8 @@ $(error FLASH required)
 endif
 
 SERIAL_PORT ?= /dev/ttyACM0
+LITTLEFS_IMAGE ?= littlefs.img
+FS_DIR ?= data
 
 VENDOR := rp2040
 PROCESSOR_FAMILY := rp2040
@@ -27,3 +29,12 @@ SUFFIX_HEX ?= uf2
 -include build-targets.mk
 
 serial.port = $(SERIAL_PORT)
+FS_PAGESIZE := 256
+FS_BLOCKSIZE := 4096
+FS_SIZE := $(shell echo $($(BOARD).menu.flash.$(FLASH).build.fs_end) " - " $($(BOARD).menu.flash.$(FLASH).build.fs_start) | bc)
+
+#  -p 256 -b 4096 -s 1048576
+$(LITTLEFS_IMAGE): $(wildcard $(FS_DIR)/*)
+	$(runtime.tools.pqt-mklittlefs.path)/mklittlefs -c $(FS_DIR) -b $(FS_BLOCKSIZE) -p $(FS_PAGESIZE) -s $(FS_SIZE) $@
+
+littlefs: $(LITTLEFS_IMAGE)
